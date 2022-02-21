c90a496d0a5e3b2471f74c83a0d1bec39790a652
armin404 <armin.omanovic@stu.ibu.edu.ba>
2022-02-21 01:30:40 +0100

    Pagination fix II

--- controllers/post_mech.js
+++ controllers/post_mech.js
@@ -21,7 +21,7 @@ exports.getPosts = asyncHandeler(async (req, res, next) => {
 
 
     //Pagination
-    const page = parseInt(req.query.page, 10) || 10;
+    const page = parseInt(req.query.page, 10) || 1;
     const limit = parseInt(req.query.limit, 10) || 10;
     const startIndex = (page - 1) * limit;
     const endIndex = page*limit;
