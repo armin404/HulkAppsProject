751cc6356a0aec0fa1e1b3e29729cbbdaba3ebb3
armin404 <92267253+armin404@users.noreply.github.com>
2022-07-13 09:17:02 +0200

    Update dataBase.js

--- config/dataBase.js
+++ config/dataBase.js
@@ -3,7 +3,7 @@ const colors = require('colors');
 
 //DB Connection setup
 const connectToDataBase = async () =>{
-    const connection = await mongoose.connect('mongodb+srv://armin404:123123gg@maincluster.2ikel.mongodb.net/HulkAppsProject?retryWrites=true&w=majority');
+    const connection = await mongoose.connect('');
     console.log(colors.bgMagenta(`MongoDB Connected: ${connection.connection.host}`));
     console.log(colors.bold('-Connection successful-'))
 };
